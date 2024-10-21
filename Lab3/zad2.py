5693781f2d1aa495b2fc4160155b38ba51bc2e86
Michał Kaczmarek <michal31kaczmarek@gmail.com>
2024-10-21 14:07:32 +0200

    lab3, zad2

--- Lab3/zad2.py
+++ Lab3/zad2.py
@@ -2,19 +2,28 @@ import cv2
 import numpy as np
 
 def trackbar_value_changed(value, winName, img):
+    value = value*2 + 1
+    kernel = np.ones((value, value), np.uint8)
     match winName:
-        case "erode":
-            img_blur = cv2.blur(img, (value, value))
-            cv2.imshow(winName, img_blur)
-        case "dilate":
-            img_blur = cv2.GaussianBlur(img, (value, value), 0)
-            cv2.imshow(winName, img_blur)
+        case "eroded":
+            new_img = cv2.erode(img, kernel, iterations=1)
+            cv2.imshow(winName, new_img)
+        case "dilated":
+            new_img = cv2.dilate(img, kernel, iterations=1)
+            cv2.imshow(winName, new_img)
 
 def main():
-    img = cv2.imread('obraz.jpeg', cv2.IMREAD_GRAYSCALE)
-    cv2.imshow('image', img)
-    _, thresh_img = cv2.threshold(img, 125, 255, cv2.THRESH_BINARY)
-    cv2.erode
+    img = cv2.imread('1.png', cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (400, 400))
+    _, thresh_img = cv2.threshold(img, 70, 255, cv2.THRESH_BINARY)
+    cv2.imshow('image', thresh_img )
+    kernel = np.ones((15, 15), np.uint8)
+    eroded = cv2.erode(thresh_img, kernel, iterations=1)
+    dilated  = cv2.dilate(thresh_img, kernel, iterations=1)
+    cv2.imshow('eroded', eroded)
+    cv2.imshow('dilated', dilated)
+    cv2.createTrackbar('kernel (2n+1)', 'eroded', 2, 100, lambda value: trackbar_value_changed(value, "eroded", thresh_img))
+    cv2.createTrackbar('kernel (2n+1)', 'dilated', 2, 100, lambda value: trackbar_value_changed(value, "dilated", thresh_img))
     k = cv2.waitKey(0)
     if k == 27:  # wait for ESC key to exit
         cv2.destroyAllWindows()
