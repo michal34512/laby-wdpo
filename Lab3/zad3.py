3366a9dcac1a2229bc35c2aa29107989726e8f8a
Michał Kaczmarek <michal31kaczmarek@gmail.com>
2024-10-21 19:32:27 +0200

    zad3

--- Lab3/zad3.py
+++ Lab3/zad3.py
@@ -1,3 +1,4 @@
+import time
 import cv2
 import numpy as np
 
@@ -16,23 +17,41 @@ def make_every_third_pixel_white():
         cv2.destroyAllWindows()
 
 def myBlur(img):
+    new_image = np.zeros(img.shape, dtype=np.uint8)
     for x in range(1, img.shape[0] - 2):
         for y in range(1, img.shape[1]  - 2):
             sum = 0. 
             for kerx in range(-1, 2):
                 for kery in range(-1, 2):
                     sum += img[x + kerx, y + kery]
-            img[x, y] = sum / 9.
+            new_image[x, y] = sum / 9.
+    return new_image
 def blurred_img():
     img = cv2.imread('obraz.jpeg', cv2.IMREAD_GRAYSCALE)
     cv2.imshow("normal", img)
-    myBlur(img)
+    img = myBlur(img)
     cv2.imshow("blur", img)
     k = cv2.waitKey(0)
     if k == 27:  # wait for ESC key to exit
         cv2.destroyAllWindows()
+def compare_durations():
+    img = cv2.imread('obraz.jpeg', cv2.IMREAD_GRAYSCALE)
+    start_time = time.perf_counter()
+    cvblur = cv2.blur(img, (3,3))
+    cvtime = time.perf_counter() - start_time
+    start_time = time.perf_counter()
+    myblur = myBlur(img)
+    mytime = time.perf_counter() - start_time
+    print(f"Cv: {cvtime} My: {mytime}")
+def filter2D():
+    img = cv2.imread('obraz.jpeg', cv2.IMREAD_GRAYSCALE)
+    start_time = time.perf_counter()
+    blurred_filter2D = cv2.filter2D(img, -1, (3,3))
+    cvfiltertime = time.perf_counter() - start_time
+    print(f"Cvfilter: {cvfiltertime}")
+    
 def main():
-    blurred_img()
+    filter2D()
     
 if __name__ == "__main__":
     main()
